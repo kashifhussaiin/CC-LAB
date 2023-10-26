ebc35fcaa1e6eb5b2cac7cf193b2cb5feb1f59ac
KASHIF HUSSAIN <113879405+kashifhussaiin@users.noreply.github.com>
2023-10-26 15:31:13 +0500

    Add files via upload

---
+++ Lab_mid/Question2(parser)/ccmid/LL1Parser.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ccmid
+{
+    internal class LL1Parser
+    {
+        private string[] tokens;
+        private int index;
+
+        public LL1Parser(string input)
+        {
+            // Tokenize the input string (split by comma)
+            tokens = input.Split(',');
+            index = 0;
+        }
+
+        public void Parse()
+        {
+            if (List())
+            {
+                if (index == tokens.Length)
+                {
+                    Console.WriteLine("Parsing successful!");
+                }
+                else
+                {
+                    Console.WriteLine("Parsing error. Unexpected tokens.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parsing error.");
+            }
+        }
+
+        private bool List()
+        {
+            if (Item())
+            {
+                return Rest();
+            }
+            return false;
+        }
+
+        private bool Rest()
+        {
+            if (index < tokens.Length && tokens[index] == ",")
+            {
+                index++;
+                if (Item())
+                {
+                    return Rest();
+                }
+                return false;
+            }
+            return true; // ε (empty string)
+        }
+
+        private bool Item()
+        {
+            if (index < tokens.Length && (tokens[index] == "id" || tokens[index] == "num" || tokens[index] == "string"))
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
---
+++ Lab_mid/Question2(parser)/ccmid/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ccmid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input string to parse
+            string input = "id, num, string, id, num";
+
+            // Create the parser
+            LL1Parser parser = new LL1Parser(input);
+
+            // Start parsing
+            parser.Parse();
+        }
+    }
+}
